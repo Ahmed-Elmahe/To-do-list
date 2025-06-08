2768f3eb171222a4973c0ce95415688098f5a36c
Ahmed-Elamhe <officialalmahui@gmail.com>
2025-06-08 17:01:03 +0300

    change in the database

--- task_project/settings.py
+++ task_project/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     path.join(BASE_DIR, 'task_project/static'),
 ]
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
