22148773a48d624c646ef0b2f5b4f80ffd9106d5
Ahmed-Elamhe <officialalmahui@gmail.com>
2025-06-08 16:56:45 +0300

    change in the database

--- task_project/settings.py
+++ task_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fjs6+s44p0^fgv*+qp+ub!c#o%d_y#ub1#edcw^7b_t566m#hp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_list_app.apps.TodoListAppConfig'
+    'todo_list_app.apps.TodoListAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'task_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "AYdbvZxPCxLAWPoBzIgcvUcSvEWBgWLR",
+        "HOST": "postgres.railway.internal",
+        "PORT": "5432",
     }
 }
 
