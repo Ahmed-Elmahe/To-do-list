c23c8765cbabc59799696f172a62c742b5818d1c
Ahmed-Elamhe <officialalmahui@gmail.com>
2025-06-08 22:53:52 +0300

    change in sttings.py file & wsgi.py file

--- task_project/wsgi.py
+++ task_project/wsgi.py
@@ -10,18 +10,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from django.conf import settings  # Import settings module
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_project.settings')
 
-# Get the base WSGI application
 base_application = get_wsgi_application()
 
-# Wrap with WhiteNoise for static files
 application = WhiteNoise(
     base_application,
-    root=settings.STATIC_ROOT,  # Uses STATIC_ROOT from settings
-    prefix=settings.STATIC_URL,  # Uses STATIC_URL from settings
-    index_file=True  # Serve index.html files for directories
+    root=settings.STATIC_ROOT,
+    prefix=settings.STATIC_URL,
+    index_file=True
 )
 
